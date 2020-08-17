4adf91e45a4d78ac7a7c7685daf3770858e64417
Florian <naumannfl79974@th-nuernberg.de>
2020-08-17 23:12:06 +0200

    Feature: Text from label input field in the spawnbox is the text  lable of a created stop watch.
    - text of input field is used as the lable text for a stop watch created.
    - input field is trimmed so text of it is trimmed for the lable of a stop watch
    - input field is emptied on creation of a stop watch for a new fresh text input

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,24 @@
 import * as DOM_Manipulation from "./Modules/DOM_Manipulation.js";
 (function() {
-        
+    /* Getting dom elements for reference. */     
     const stop_watch_wrapper = document.querySelector("#stop-watch-list");
-    document.querySelector("#SpawnManage-btnS").addEventListener("click", () => {
-        let stopWatch = DOM_Manipulation.createStopWatch("Stop-Watch 2", "20:20:20");
-        callbackClickTrashButton(stopWatch);
+    const inputFieldLableStopWatch = document.querySelector("#InputFieldLableStopWatch");
+    
+    /* Attaching events */
+    // Attaching events for the spawn watch box
+        
+    // Adding event for spawn manage button
+    document.querySelector("#SpawnManage-btnS").addEventListener("click", () => {        
+        let stopWatch = DOM_Manipulation.createStopWatch(inputFieldLableStopWatch.value.trim(), "20:20:20");
+        inputFieldLableStopWatch.value = "";
+        AddCallbackClickTrashButton(stopWatch);
         stop_watch_wrapper.appendChild(stopWatch);
     });
+    
+    /* Functions */
 
     // Adds a click event with a callback function to the trash button of a stop watch. 
-    function callbackClickTrashButton(stopWatch) {
+    function AddCallbackClickTrashButton(stopWatch) {
         let trashButton = stopWatch.querySelector(".trash-btn");
         trashButton.addEventListener("click", () => {
             DOM_Manipulation.RemoveStopWatchViaTrashButton(trashButton);
