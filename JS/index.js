d8f9b01a973d9e8028bb36d293a06fd10d67e4ec
Florian <naumannfl79974@th-nuernberg.de>
2020-08-17 22:12:19 +0200

    Added click event and callback function on the plus button in the spawn for stop watches.
    When clicked it spawns a new stop watch.

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,12 @@
 import { testString, createStopWatch } from "./Modules/DOM_Manipulation.js";
 (function() {
+        
+    const stop_watch_wrapper = document.querySelector("#stop-watch-list");
+    document.querySelector("#SpawnManage-btnS").addEventListener("click", () => {
+        stop_watch_wrapper.appendChild(createStopWatch("Stop-Watch 2", "20:20:20"));
+    });
+
     
-    let stop_watch_wrapper = document.querySelector("#stop-watch-list");
-    stop_watch_wrapper.appendChild(createStopWatch("Stop-Watch", "20:20:20"));
+    
     
 }) ()
