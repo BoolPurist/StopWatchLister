22873f892a31a454d4922f5b9f0a7fe109a2c172
Florian <naumannfl79974@th-nuernberg.de>
2020-08-22 05:20:52 +0200

    Implementation: Stop watch can be started and deletet

--- JS/Modules/StopWatch.js
+++ JS/Modules/StopWatch.js
@@ -1,10 +1,118 @@
 // @ts-check
+import {Timer } from "./Timer.js";
 
-class StopWach {
+class StopWatch {
 
-    constructor() {
+    /**
+     * 
+     * @param {!string} whereToAppend - valid querySelector
+     * @param {!string} querySelectorForTimeStamp - valid querySelector to find the dom element to 
+     * apply current time regularally once started 
+     * @param {object} [DomSubReferences]
+     */
+    constructor(whereToAppend, querySelectorForTimeStamp , ...DomSubReferences) {
+        this.domReference = GetContainerForStopWatch().children[0];
+        
+        this._timer = new Timer();
+        this._timeStampField = this._GetDomSubReference(querySelectorForTimeStamp);
+        this._timeStampField.textContent = this._timer.TimeStamp;
+        this._timer.addFuncOnChange(StopWatch._callbackUpdateTimeStamp, this);
+        this._timerIsActive = false;
+        
+        for ( const dynamicProperty of DomSubReferences ) {
+            this._CreateSubElementReference(dynamicProperty);
+        }
+
+        // this._attachAllDefaultEvents();
+
+        document.querySelector(whereToAppend).appendChild(this.domReference);
+    }
+
+
+    static _callbackUpdateTimeStamp(stopWatchRef) {
+        stopWatchRef = stopWatchRef[0];
+        stopWatchRef._timeStampField.textContent = stopWatchRef._timer.TimeStamp;
+    }
+
+    remove() {
+        this._timer.stop();
+        this.domReference.remove();
+    }
+
+    start() {
+        if (this._timerIsActive === false) {
+            this._timer.start();
+            this._timerIsActive = true;
+    
+        }
+    }
+
+    _GetDomSubReference (querySelector) {
+        return this.domReference.querySelector(querySelector);
+    }
+
+    _CreateSubElementReference({ propertyName, domQuerySelector }) {
+        Object.defineProperty(this, propertyName, {
+            value: this.domReference.querySelector(domQuerySelector),
+            writable: false
+        });
         
     }
+    
+    _PopulateDomElementWithTextContent (...Data) {
+        Data.forEach(object => {
+            const {querySelector, textContent} = object;
+            this._GetDomSubReference(querySelector).textContent = textContent;
+        })
+    }
+
+    _attachAllDefaultEvents () {
+        for (const defaultEventHandler of StopWatch.prototype._defaultEventHandler) {
+            
+            const domElementReference = this.domReference.querySelector(
+                defaultEventHandler.domElementReference
+            );
+
+            const eventType = defaultEventHandler.eventType; 
+            const callbackFunction  = defaultEventHandler.callbackFunction ; 
+
+            domElementReference.addEventListener(eventType, callbackFunction, false);
+        }
+    }
+
 }
 
-export { StopWach }
+StopWatch.prototype._defaultEventHandler = [];
+
+function GetContainerForStopWatch()  {
+    const container = document.createElement("div");
+
+    container.innerHTML = `
+<div class="stop-watch">
+    <div class="stop-watch-row-label">
+        <p class="stop-watch-label-text"></p>
+        <i class="trash-btn fas fa-trash-alt"></i>
+    </div>
+    <div class="stop-watch-row-timer">
+        <i class="play-btn fas fa-play"></i>
+        <p class="text-timer">23:54:02</p>
+    </div>
+</div>
+`;
+
+    return container;
+}
+
+// <!-- html structure for StopWatch -->
+// <div class="stop-watch">
+//     <div class="stop-watch-row-label">
+//         <p class="stop-watch-label-text">1. Stop-Watch</p>
+//         <i class="trash-btn fas fa-trash-alt"></i>
+//     </div>
+//     <div class="stop-watch-row-timer">
+//         <i class="play-btn fas fa-play"></i>
+//         <p class="text-timer">23:54:02</p>
+//     </div>
+// </div>
+
+export { StopWatch }
--- JS/index.js
+++ JS/index.js
@@ -1,36 +1,83 @@
 
 import * as DOM_Manipulation from "./Modules/DOM_Manipulation.js";
-import {Timer} from "./Modules/Timer.js";
+import { StopWatch } from "./Modules/StopWatch.js";
+
 
 (function() {
     "use strict";
-    /* Getting dom elements for reference. */     
-    const stop_watch_wrapper = document.querySelector("#stop-watch-list");
-    const inputFieldLableStopWatch = document.querySelector("#InputFieldLableStopWatch");
+    // SW = stopwatch
+    // QS = querySelector
     
+    // querySelector strings for accessing certain dom elements 
+    const QSListSW = "#stop-watch-list";
+    const QSSpawnBtn = "#spawn-btn";
+
+    const QSClassTextTimer = ".text-timer";
+    const QSPlayBtn = ".play-btn";
+    const QSTrashBtn = ".trash-btn";
+    const QSLableTextSW = ".stop-watch-label-text";
+
+    const inputFieldLableStopWatch = document.querySelector("#InputFieldLableStopWatch");
+    /**
+     * @type {Array<StopWatch>} - array of stopwatches wich are in the dom currently
+     */
+    const stopWatchList = [];
+    
+    // Names of the properties which are created on every stop watch at runtime
+    const playBtnPropertyName = "playButton";
+    const deleteButton = "trashButton";
+
+
     /* Attaching events */
     // Attaching events for the spawn watch box    
     // Adding event for spawn manage button
     
-    document.querySelector("#SpawnManage-btnS").addEventListener("click", () => {    
-            
-        let stopWatch = DOM_Manipulation.createStopWatch(inputFieldLableStopWatch.value.trim(), "20:20:20");
+    document.querySelector(QSListSW).addEventListener("click", (event) => {
+        if ( event.target.className.includes(QSPlayBtn.substring(1)) ) {
+            for (const stopWatch of stopWatchList) {
+                if (stopWatch[playBtnPropertyName] === event.target) {
+                    stopWatch.start();
+                }                
+            }
+        } else if (event.target.className.includes(QSTrashBtn.substring(1))) {
+            for (const stopWatch of stopWatchList) {
+                if (stopWatch[deleteButton] === event.target) {                    
+                    stopWatch.remove();                    
+                }                
+            }                                    
+        }
+    });
+
+    document.querySelector(QSSpawnBtn).addEventListener("click", () => {    
+        const lableText = inputFieldLableStopWatch.value.trim();
         inputFieldLableStopWatch.value = "";
-        AddCallbackClickTrashButton(stopWatch);
-        stop_watch_wrapper.appendChild(stopWatch);
+        
+        const stopWatch = new StopWatch(
+            QSListSW,
+            QSClassTextTimer, 
+            { propertyName: playBtnPropertyName, domQuerySelector: QSPlayBtn },                       
+            { propertyName: deleteButton, domQuerySelector:  QSTrashBtn},                       
+        );
+
+        stopWatchList.push( stopWatch ); 
+        populateDomElementWithTextContent(
+            stopWatch.domReference,
+            {querySelector: QSLableTextSW, textContent: lableText}
+        );
+        
+        
     });
     
     /* Functions */
-
-    const timer = new Timer();
     
-    // Adds a click event with a callback function to the trash button of a stop watch. 
-    function AddCallbackClickTrashButton(stopWatch) {
-        let trashButton = stopWatch.querySelector(".trash-btn");
-        trashButton.addEventListener("click", () => {
-            DOM_Manipulation.RemoveStopWatchViaTrashButton(trashButton);
-        })
+    function populateDomElementWithTextContent (startDomElement, ...Data) {
+        
+        Data.forEach(
+            object => {
+            const {querySelector, textContent} = object;
+            startDomElement.querySelector(querySelector).textContent = textContent;
+            }
+        );
     }
-    
     
 }) ()
